bcead0231d599b2975f9598555bb8da2de646db2
jgmotors-crm <jg.motorsportt@gmail.com>
2025-06-08 23:06:59 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+// src/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -6,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDpPENfwagrXub2AuosZpo2fCy3kQpzp9M",
   authDomain: "jg-motors-crm.firebaseapp.com",
   projectId: "jg-motors-crm",
-  storageBucket: "jg-motors-crm.firebasestorage.app",
+  storageBucket: "jg-motors-crm.appspot.com",
   messagingSenderId: "488627812693",
   appId: "1:488627812693:web:6c14c9aaa9b2076d3d57dc",
   measurementId: "G-YFCDX1HWYH"
